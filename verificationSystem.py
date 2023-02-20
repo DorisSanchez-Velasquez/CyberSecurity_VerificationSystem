a0038c59ecccb6d749e2a71aa5ec720b47e701f0
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-02-20 12:19:38 -0500

    Created skeletons for verification system functions and their purpose

--- verificationSystem.py
+++ verificationSystem.py
@@ -1,1 +1,18 @@
-print("Yolo")
+## IMPORTS -------------------------------------------------------
+import hashlib
+import linecache
+
+##VERIFICATION SYSTEM FUNCTIONS -----------------------------------
+#This function opens the password.txt and salt.txt files to read the first lines to obtain salt and password values
+#per assignment requirements
+def fileExtraction():
+    return
+
+#This function computes the hash value of a concatenated combination of a given password and salt value
+def computeMD5hash():
+    return
+
+#This function verifies the hash value by extraction given a specific user id and compares this with computed hash value
+def verifyHash():
+    return
+
