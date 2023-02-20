d7aa72389ef0137d6ab7a738cc65b54fbf0768ea
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-02-20 12:26:28 -0500

    Updated fileExtraction function to obtain password and salt values from given text files

--- verificationSystem.py
+++ verificationSystem.py
@@ -6,7 +6,19 @@ import linecache
 #This function opens the password.txt and salt.txt files to read the first lines to obtain salt and password values
 #per assignment requirements
 def fileExtraction():
-    return
+    #Obtain first password for user id 1
+    pw_file = open("Password.txt", "r")
+    password = pw_file.readline()
+    pw_file.close()
+
+    #Obtain first salt value for user id 1
+    salt_file = open("Salt.txt", "r")
+    salt = salt_file.readline()
+    salt_file.close()
+
+    #Strip the password and salt of any new lines/spaces and return concatenation
+    concat_txt = password.strip() + salt.strip()
+    return concat_txt
 
 #This function computes the hash value of a concatenated combination of a given password and salt value
 def computeMD5hash():
